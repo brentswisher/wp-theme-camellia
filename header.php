380423ad4eebb9745b0d3691687616a5edc04ff2
Brent Swisher <swisherb@gvsu.edu>
2018-09-17 21:44:17 -0400

    enable custom header

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'camellia' ); ?></a>
-
+	<?php the_custom_header_markup(); ?>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
